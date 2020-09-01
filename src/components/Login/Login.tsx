3b6759486f4394a3d45a7a77d68e9fd0d536f54f
Andriy <refwol131@mail.ru>
2020-09-02 00:48:16 +0300

    redux-form 75 done

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,10 +1,47 @@
 import React from "react"
+import {reduxForm, Field, InjectedFormProps} from "redux-form";
 
-const Login = () => {
+
+type FormDataType = {
+    login: string
+    password: string
+    rememberMe: boolean
+}
+
+const LoginForm = (props: InjectedFormProps<FormDataType>) => {
+
     return (
-        <>
-            <h1>LOGIN</h1>
-        </>
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field type="text" placeholder={"Login"} name={"login"} component={"input"}/>
+            </div>
+            <div>
+                <Field type="text" placeholder={"Password"} name={"password"} component={"input"}/>
+            </div>
+            <div>
+                <Field component={"input"} type="checkbox" name={"rememberMe"}/> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
     )
 }
+
+const LoginReduxForm = reduxForm<FormDataType>({form: 'login'})(LoginForm)
+
+const Login = () => {
+
+    const onSubmit = (formData: FormDataType) => {
+        console.log(formData)
+    }
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <LoginReduxForm onSubmit={onSubmit}/>
+        </div>
+    )
+}
+
 export default Login
